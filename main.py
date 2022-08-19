ca3ca6157fd15b579c58da5441ffd79b50823088
root <root>
2022-08-19 11:29:22 -0600

    Initial imports

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import tkinter as tk
+
+border_effects = {
+    "flat": tk.FLAT,
+    "sunken": tk.SUNKEN,
+    "raised": tk.RAISED,
+    "groove": tk.GROOVE,
+    "ridge": tk.RIDGE,
+}
+
+window = tk.Tk()
+frame = tk.Frame()
+frame.pack()
+
+window.mainloop()
+
+
+
